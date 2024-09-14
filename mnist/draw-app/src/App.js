dd53b12758244173e3e2981b1cf6cb135811a1c3
Angie Baik <23angieb@berkeley.edu>
2024-09-14 14:03:55 -0700

    set up until file-saver

---
+++ mnist/draw-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Classify Digits</h1>
+    </div>
+  );
+}
+
+export default App;
