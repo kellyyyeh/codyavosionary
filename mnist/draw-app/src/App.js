9dcf19d7d40f4c4ce0b093379a403c8fc68bc675
Kelly Yeh <kellyyyeh@berkeley.edu>
2024-09-14 14:22:37 -0700

    setup up to SketchField

--- mnist/draw-app/src/App.js
+++ mnist/draw-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Draw from './components/Draw';
 
 function App() {
   return (
     <div className="App">
       <h1>Classify Digits</h1>
+      <Draw />
     </div>
   );
 }
