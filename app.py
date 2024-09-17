6b5587f4ca446a59ed54e8a0a537aacb786dee9d
Angie Baik <23angieb@berkeley.edu>
2024-09-17 10:59:37 -0700

    Resolved merge conflicts

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import (
+    Flask, render_template, request, redirect, url_for, session)
+from tensorflow import keras
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/recognize', methods=['Get'])
+def recognize_get():
+    return render_template('recognize.html')
+
+@app.route('/recognize', methods=['POST'])
+def recognize_post():
+    pixels = request.form['pixels']
+    pixels = pixels.split(',')
+    img = np.array(pixels).astype(float).reshape(1, 50, 50, 1)
+    print('before loading')
+
+    model = keras.models.load_model('number.keras')
+
+    pred = np.argmax(model.predict(img), axis=-1)
+    print(pred)
+    return render_template('recognize.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
