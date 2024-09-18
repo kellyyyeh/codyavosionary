bde964fe634be9131dc33f8d5f78314aa1e89278
Kelly Yeh <kellyyyeh@berkeley.edu>
2024-09-18 14:07:29 -0700

    separated drawing board from cnn, drawing board split into four quardrants

--- static/index.js
+++ static/index.js
@@ -67,6 +67,24 @@ function addClick(x, y, dragging) {
 
 function clearCanvas() {
   context.clearRect(0, 0, 200, 200)
+
+  // Get the quadrant canvases
+  const topLeft = document.getElementById("top-left");
+  const topRight = document.getElementById("top-right");
+  const bottomLeft = document.getElementById("bottom-left");
+  const bottomRight = document.getElementById("bottom-right");
+
+  // Get their contexts
+  const ctxTopLeft = topLeft.getContext("2d")
+  const ctxTopRight = topRight.getContext("2d")
+  const ctxBottomLeft = bottomLeft.getContext("2d")
+  const ctxBottomRight = bottomRight.getContext("2d")
+
+  // Clear each quadrant canvas
+  ctxTopLeft.clearRect(0, 0, topLeft.width, topLeft.height)
+  ctxTopRight.clearRect(0, 0, topRight.width, topRight.height)
+  ctxBottomLeft.clearRect(0, 0, bottomLeft.width, bottomLeft.height)
+  ctxBottomRight.clearRect(0, 0, bottomRight.width, bottomRight.height)
 }
 
 function resetCanvas() {
@@ -78,7 +96,7 @@ function resetCanvas() {
 
 function drawCanvas() {
   clearCanvas()
-
+  
   for(let i=0; i<clickX.length; i++) {
     context.beginPath()
     if (clickDrag[i] && i) {
@@ -90,6 +108,31 @@ function drawCanvas() {
     context.closePath()
     context.stroke()
   }
+
+  displayQuadrants();
+}
+
+function displayQuadrants() {
+  const halfWidth = canvas.width / 2
+  const halfHeight = canvas.height / 2
+
+  // Get the image data for each quadrant
+  const topLeftData = context.getImageData(0, 0, halfWidth, halfHeight)
+  const topRightData = context.getImageData(halfWidth, 0, halfWidth, halfHeight)
+  const bottomLeftData = context.getImageData(0, halfHeight, halfWidth, halfHeight)
+  const bottomRightData = context.getImageData(halfWidth, halfHeight, halfWidth, halfHeight)
+
+  // Get contexts for each quadrant canvas
+  const topLeftCanvas = document.getElementById("top-left").getContext("2d")
+  const topRightCanvas = document.getElementById("top-right").getContext("2d")
+  const bottomLeftCanvas = document.getElementById("bottom-left").getContext("2d")
+  const bottomRightCanvas = document.getElementById("bottom-right").getContext("2d")
+
+  // Draw the image data onto the respective canvases
+  topLeftCanvas.putImageData(topLeftData, 0, 0)
+  topRightCanvas.putImageData(topRightData, 0, 0)
+  bottomLeftCanvas.putImageData(bottomLeftData, 0, 0)
+  bottomRightCanvas.putImageData(bottomRightData, 0, 0)
 }
 
 // this is shrinking the image, but still grabbing the important parts of it
